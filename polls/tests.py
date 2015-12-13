cf8ec78f1225e8c28bb7b8ca91a0fed14d382fda
czqsole <czqsole@gmail.com>
2015-12-14 00:57:53 +0800

    05
    编写TEST

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,58 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from .models import Question
+
+
+'''
+    python manage.py test polls
+
+    What happened is this:
+
+        python manage.py test polls looked for tests in the polls application
+        it found a subclass of the django.test.TestCase class
+        it created a special database for the purpose of testing
+        it looked for test methods - ones whose names begin with test
+        in test_was_published_recently_with_future_question it created a Question instance whose pub_date field is 30 days in the future
+        ... and using the assertEqual() method, it discovered that its was_published_recently() returns True, though we wanted it to return False
+        The test informs us which test failed and even the line on which the failure occurred.
+'''
+
+def create_quesiton(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_piblished_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+class QuestionViewTests(TestCase):
+
+    def test_index_view_with_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertCountEqual(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_a_last_quesiton(self):
+        create_quesiton(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_index_view_with_a_future_question(self):
+        create_quesiton(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertCountEqual(response, "No polls are available.",
+                              status_code=200)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
 # Create your tests here.
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+
 
 from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+
+        #return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 
 def index(request):
@@ -20,6 +44,7 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
